602dcbf4369c909db2f3324bb31ce725352a0ff7
QMCCC <2868916930@qq.com>
2025-10-20 09:57:58 +0800

    着色器定义

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -2,6 +2,15 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+static unsigned int CompileShader(const std::string& source, unsigned int type) {
+
+}
+
+static int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = glCreateShader(GL_VERTEX_SHADER);
+}
+
 int main(void)
 {
     GLFWwindow* window;
